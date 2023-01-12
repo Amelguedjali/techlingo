51002050e9eca29fb4728534ed2bf8a002e0256f
Mirabdf <91785113+Mirabdf@users.noreply.github.com>
2023-01-12 16:42:16 +0100

    Add files via upload

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 Color blueC = Color(0xFF2F89FC);
 Color blueF = Color(0xFF294F81);
 Color white = Color(0xFFD9D9D9);
@@ -10,3 +11,9 @@ Color red = Color(0xFFCC0202);
 Color color2 = Color(0xFFF5F5F5);
 Color color3 = Color(0xFFEDEDED);
 Color color4 = Color(0xFFFAFAFA);
+Color color5 = Color(0xFFF8F8F8);
+
+
+
+
+
